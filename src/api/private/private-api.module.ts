4708b00b0e78cc62e6f84880142d03f3ecb22512
Philip Molares <philip.molares@udo.edu>
2021-03-05 00:30:47 +0100

    PrivateAPI: Add me controller
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/api/private/private-api.module.ts
+++ src/api/private/private-api.module.ts
@@ -9,12 +9,21 @@ import { TokensController } from './tokens/tokens.controller';
 import { LoggerModule } from '../../logger/logger.module';
 import { UsersModule } from '../../users/users.module';
 import { AuthModule } from '../../auth/auth.module';
+import { MeController } from './me/me.controller';
+import { MediaModule } from '../../media/media.module';
 import { HistoryController } from './me/history/history.controller';
 import { HistoryModule } from '../../history/history.module';
 import { NotesModule } from '../../notes/notes.module';
 
 @Module({
-  imports: [LoggerModule, UsersModule, AuthModule, HistoryModule, NotesModule],
-  controllers: [TokensController, HistoryController],
+  imports: [
+    LoggerModule,
+    UsersModule,
+    AuthModule,
+    HistoryModule,
+    NotesModule,
+    MediaModule,
+  ],
+  controllers: [TokensController, HistoryController, MeController],
 })
 export class PrivateApiModule {}
